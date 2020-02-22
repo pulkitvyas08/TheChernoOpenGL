bd1fd98ccc4ee0e79f62e48ae1986976924e03a3
pulkitvyas08 <e19cse284@bennett.edu.in>
2020-02-22 23:23:20 +0530

    episode 15

---
+++ lec_one/src/Shader.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+
+class Shader
+{
+private:
+	std::string m_FilePath;
+	unsigned int m_rendererID;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
+public:
+	Shader(const std::string& filepath);
+	~Shader();
+
+	void Bind() const;
+	void UnBind() const;
+
+	//set uniforms
+	void SetUniform4f(const std::string& name, float v0, float v1, float f2, float f3);
+	void SetUniform1f(const std::string& name, float value);
+private:
+	unsigned int GetUniformLocation(const std::string& name);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+	unsigned int CompileShader(unsigned int type, const std::string& source);
+	ShaderProgramSource ParseShader(const std::string& filepath);
+};
