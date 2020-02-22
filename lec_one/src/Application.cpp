14d9824360097694fd04fd7b2dea77440933d7cc
pulkitvyas08 <e19cse284@bennett.edu.in>
2020-02-22 15:31:01 +0530

    episode 11

--- lec_one/src/Application.cpp
+++ lec_one/src/Application.cpp
@@ -125,6 +125,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
         std::cout << "ERROR" << std::endl;
     
@@ -156,22 +158,30 @@ int main(void)
     GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-    std::cout << "VERTEX" << std::endl;
-    std::cout << source.VertexSource << std::endl;
-    std::cout << "FRAGMENT" << std::endl;
-    std::cout << source.FragmentSource << std::endl;
-
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
 
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
+
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
     
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr)) ;
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
         
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0)
+            increment = 0.05f;
+
+        r += increment;
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
