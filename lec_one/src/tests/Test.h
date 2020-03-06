c9308dcee41d5d96a587d1af0ce95c355a9b4ae9
pulkitvyas08 <e19cse284@bennett.edu.in>
2020-03-06 21:28:48 +0530

    episode 25 & 26

--- lec_one/src/tests/Test.h
+++ lec_one/src/tests/Test.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <functional>
+#include <vector>
+#include <string>
+#include <iostream>
+
 namespace test
 {
 	class Test
@@ -8,8 +13,27 @@ namespace test
 		Test() {}
 		virtual ~Test() {}
 
-		virtual void OnUpdate(float delatTime) {}
+		virtual void OnUpdate(float deltaTime) {}
 		virtual void OnRender() {}
 		virtual void OnImGuiRender() {}
+	};
+
+	class TestMenu : public Test
+	{
+	public:
+		TestMenu(Test*& currentTestPointer);
+	
+		void OnUpdate(float deltaTime) override;
+
+		template<typename T>
+		void RegisterTest(const std::string& name)
+		{
+			std::cout << "Registering test " << name << std::endl;
+
+			m_Tests.push_back(std::make_pair(name, [] { return new T; }));
+		}
+	private:
+		Test*& m_CurrentTest;
+		std::vector<std::pair<std::string, std::function<Test * ()>>> m_Tests;
 	};
 }	
